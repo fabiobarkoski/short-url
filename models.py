9898f3ce18145fcab24dd34b16ca86cb4de083c4
fabiobarkoski <fabio0barkoski@gmail.com>
2021-07-26 13:06:11 -0300

    to heroku v5

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class Short(db.Entity):
     original_link = Required(str)
     short_link = Required(str)
 
-db.bind(provider='', user='', password='', host='', database='')
+db.bind(provider='postgres', user='bzclzhideovtes', password='ef5e43b3e89d0bf8871d841a86be6b8bcf63b86f4e47bd3a31f444f15106d851', host='ec2-23-21-4-7.compute-1.amazonaws.com', database='df26bllk2v7ho6')
 
 
 db.generate_mapping(create_tables=True)
@@ -15,7 +15,7 @@ set_sql_debug(True)
 
 @db_session
 def save(original_link, short_link):
-    Short(original_link=original_link, short_link='your.domain/'+short_link)
+    Short(original_link=original_link, short_link=short_link)
 
 @db_session
 def search_by_original_link(link):
