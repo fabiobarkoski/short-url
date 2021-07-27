66b643026e604f3710fa830793a3becdcaf17a4a
fabiobarkoski <fabio0barkoski@gmail.com>
2021-07-27 10:37:25 -0300

    update comments and gitignore

--- tests/test_get.py
+++ tests/test_get.py
@@ -1,20 +1,28 @@
 import pytest
 import requests
 
+
 def test_redirection():
+    '''
+    Função que testa o redirecionamento da API
+    '''
     url = 'http://127.0.0.1:8000/RA1UXjh'
 
     response = requests.get(url)
     if response.status_code == 200:
-        assert response.text != None
+        assert response.text is not None
     else:
         assert False
 
+
 def test_home_info():
+    '''
+    Função que testa o acesso ao bem-vindo da API
+    '''
     url = 'http://127.0.0.1:8000/'
 
     response = requests.get(url)
     if response.status_code == 200:
-        assert response.text != None
+        assert response.text is not None
     else:
-        assert False        
+        assert False
--- tests/test_post.py
+++ tests/test_post.py
@@ -2,12 +2,16 @@ import pytest
 import requests
 import json
 
+
 def test_data_insert():
+    '''
+    Função que testa a inserção de dados da API
+    '''
     url = 'http://127.0.0.1:8000/'
 
-    new_link = {'link' : 'test.com'}
+    new_link = {'link': 'test.com'}
 
-    response = requests.post(url, data = json.dumps(new_link))
+    response = requests.post(url, data=json.dumps(new_link))
     response_dict = response.json()
 
     if response.status_code == 200:
@@ -16,4 +20,4 @@ def test_data_insert():
 
         assert status == 'success' and short_link is not None
     else:
-        assert False    
+        assert False
